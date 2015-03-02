1bb224efddee8744a50854dc52f7cf7fd89670b2
Philip Munksgaard <pmunksgaard@gmail.com>
2015-03-02 13:56:02 +0100

    Update test with method calls

--- examples/test.rs
+++ examples/test.rs
@@ -11,14 +11,26 @@ fn main() {
     let x = 1u8;
     let mut y = 2u8;
     y = x;
-    let mut w = Baz(1);
-    let z = Bar;
-    w = Bar;
-    let properly_dropped = Bar;
-    not_allowed(w);
-    allowed(z);
-    drop_properly(properly_dropped);
-    // should error here about w/z not being dropped properly
+    {
+        let mut w = Baz(1);
+        let z = Bar;
+        w = Bar;
+        let properly_dropped = Bar;
+        not_allowed(w);
+        allowed(z);
+        drop_properly(properly_dropped);
+        // should error here about w/z not being dropped properly
+    }
+
+    {
+        let w = Bar;
+        let properly_dropped = Bar;
+        let z = Bar;
+
+        w.not_allowed();
+        properly_dropped.proper_drop();
+        z.allowed();
+    }
 }
 
 use Foo::*;
@@ -28,6 +40,22 @@ enum Foo {
     Bar, Baz(u8)
 }
 
+impl Foo {
+    fn not_allowed(self) {
+
+    }
+
+    #[allowed_on_protected]
+    fn allowed(self) {
+
+    }
+
+    #[allowed_drop]
+    fn proper_drop(self) {
+
+    }
+}
+
 
 fn not_allowed(_: Foo) {
 
@@ -35,10 +63,10 @@ fn not_allowed(_: Foo) {
 
 #[allowed_on_protected]
 fn allowed(_: Foo) {
-    
+
 }
 
 #[allowed_drop]
 fn drop_properly(_: Foo) {
-    
+
 }
