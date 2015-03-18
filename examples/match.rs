ada425bc2c4ce923c91e67b9738c7eca3adfe869
Philip Munksgaard <pmunksgaard@gmail.com>
2015-03-18 14:07:17 +0100

    Add some simple examples

---
+++ examples/match.rs
@@ -0,0 +1,30 @@
+#![feature(plugin)]
+#![feature(custom_attribute)]
+#![plugin(humpty_dumpty)]
+#![allow(unused_attributes)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+#[drop_protect]
+struct Foo;
+
+#[drop_protect]
+struct Bar;
+
+impl Foo {
+    #[allow_drop(Foo)]
+    fn close(self) { }
+}
+
+impl Bar {
+    #[allow_drop(Bar)]
+    fn close(self) { }
+}
+
+fn main() {
+    let x: Result<Foo, Bar> = Ok(Foo);
+    match x {
+        Ok(y) => y.close(),
+        Err(y) => y.close(),
+    }
+}
