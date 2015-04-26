83ca905ac2f3b3081ba3b2e0a4ec25155642a3c4
Thomas Jespersen <laumann.thomas@gmail.com>
2015-04-26 21:48:24 +0200

    Add simple non-linear example with match statements

---
+++ tests/compile-fail/match-not-linear.rs
@@ -0,0 +1,38 @@
+#![feature(plugin)]
+#![feature(custom_attribute)]
+#![plugin(humpty_dumpty)]
+#![allow(unused_attributes)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {}
+
+#[drop_protect]
+struct Foo;
+
+impl Foo {
+    fn new() -> Foo { Foo }
+    fn one_or_the_other(self, b: bool) -> Result<Foo, Foo> {
+        if b { Ok(self) } else { Err(self) }
+    }
+    #[allow_drop="Foo"]
+    fn drop(self) {}
+}
+
+fn one() {
+    let foo = Foo::new();
+    match foo.one_or_the_other(true) {
+        //~^ ERROR Match arms are not linear
+        Ok(foo) => foo.drop(),
+        Err(foo) => {} //~ ERROR dropped var
+    }
+}
+
+fn the_other() {
+    let foo = Foo::new();
+    match foo.one_or_the_other(false) {
+        //~^ ERROR Match arms are not linear
+        Ok(foo) => {},
+        Err(foo) => foo.drop()
+    }
+}
