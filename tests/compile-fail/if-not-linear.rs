81cc6cbaa261c6e99070c7096a46924c26fa54f7
Thomas Jespersen <laumann.thomas@gmail.com>
2015-04-26 21:25:25 +0200

    Compile-fail tests for simple non-linear if-statements

---
+++ tests/compile-fail/if-not-linear.rs
@@ -0,0 +1,31 @@
+#![feature(plugin)]
+#![feature(custom_attribute)]
+#![plugin(humpty_dumpty)]
+#![allow(unused_attributes)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+#[drop_protect]
+struct Foo;
+
+impl Foo {
+    #[allow_drop="Foo"]
+    fn close(self) { }
+}
+
+fn test1() {
+    let f = Foo; //~ ERROR dropped var
+    if false { return } else { f.close(); }
+    //~^ ERROR Branch arms are not linear
+}
+
+fn test2() {
+    let f = Foo;
+    if true { f.close(); } else { return }
+    //~^ ERROR Branch arms are not linear
+}
+
+fn main() {
+    test1();
+    test2();
+}
